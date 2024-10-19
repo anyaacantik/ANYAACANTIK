c1fac0f2c7f74c28f59bf7e0617c2b64c7115618
anyaacantik <nopelcans15424@gmail.com>
2024-10-19 08:07:18 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'AKU MAU MINTA MAAF SAYANG'.split('')
+const text = 'AKU MINTA MAAF SAYANG'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
